f4233bdb714e4967319d697abf3a2ad9c2da8726
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2024-03-19 09:33:38 +0530

    Delete cms directory

--- cms/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from . import views
-from .models import Staff
-# Create your views here.
-def index (request):
-    staffs=Staff.objects.all
-    return render(request,"index.html",{'staffs':staffs})
