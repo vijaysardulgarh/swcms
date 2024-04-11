5ff6776f4473e05865e2d3c883d31c3621d84091
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2024-04-11 18:39:41 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
+    "/var/www/static/",
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
