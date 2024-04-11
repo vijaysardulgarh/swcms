01fa893adf6ed6b3ee316d15978b8611cf6ea3a9
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2024-04-11 21:10:43 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -118,12 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-#STATICFILES_DIRS = [
-#    BASE_DIR / "staticfiles",
-#]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")  # Define a separate directory for collected static files.
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')  # Specify additional directories for static files.
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
