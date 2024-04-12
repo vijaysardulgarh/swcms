164549e9515c8c86d699a466fe69541062655454
vijaysardulgarh <65865175+vijaysardulgarh@users.noreply.github.com>
2024-04-12 08:22:30 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'school.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/vijaysardulgarh/sims/db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
