ab2ee409ada8c4f834953d6842c6c6000b76107f
Vijay Kumar <vijaysardulgarh@gmail.com>
2024-05-17 13:17:12 +0530

    update

--- cms/views.py
+++ cms/views.py
@@ -90,26 +90,7 @@ def student_strength(request):
         return render(request, 'school_student_strength.html',context)
     
 def subject_strength(request):
-
-
-    subjects_opted =Student.objects.filter(school_name=school_name)       
-    if subjects_opted:
-        # Split the subjects_opted string into individual subjects
-        subject_list = subjects_opted.split(',')
-        
-        # Initialize lists to store optional and compulsory subjects
-        subjects = []
-        
-        # Iterate through each subject to categorize as optional or compulsory
-        for subject in subject_list:
-            subject_type, subject_name = subject.split(':')
-            subjects.append(subject_name.strip())  # Append to optional subjects list
-        
-   
-
-
-
-
+    
     if request.method == 'POST':
             # Get the school name from the form
             school_name = request.POST.get('school_name')
